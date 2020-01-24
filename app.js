31ba3547ab8bd12c890242114c55be6aca60e402
Harfi <harfialpharaby@gmail.com>
2020-01-24 14:53:49 +0700

    add new files for testing

--- app.js
+++ app.js
@@ -2,19 +2,25 @@
 
 const express = require('express');
 const app = express();
-const PORT = 3000;
-const mongoose = require('mongoose');
-const cors = require('cors');
+// const PORT = 3000;
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+// app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
 
-mongoose.connect('mongodb://localhose:27017/ularkadut', 
-{useNewUrlParser: true, useUnifiedTopology: true}, function() {
-    console.log(`Connected to MongoDB`);
-})
+// mongoose.connect('mongodb://localhose:27017/ularkadut', 
+// {useNewUrlParser: true, useUnifiedTopology: true}, function() {
+//     console.log(`Connected to MongoDB`);
+// })
 
-app.listen(PORT, () => {
-    console.log(`App listening on port = ${PORT}`);
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+http.listen(process.env.PORT || 3000, () => {
+    console.log(`App listening on port = ${process.env.PORT || 3000}`);
 })
