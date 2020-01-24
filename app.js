e6e1ed6b655c2322a5efe14301e998dcc8afd80c
azputra <ahmadzp102@gmail.com>
2020-01-24 09:31:44 +0700

    tambah fitur chat server listen client and send to client

--- app.js
+++ app.js
@@ -5,16 +5,25 @@ const app = express();
 const PORT = 3000;
 const mongoose = require('mongoose');
 const cors = require('cors');
+const server = require('http').createServer(app)
+const io = require('socket.io').listen(server)
+
+io.on('connection', function (socket) {
+    console.log(socket.id)
+    socket.on('SEND_MESSAGE', function (data) {
+        io.emit('MESSAGE', data)
+    });
+});
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhose:27017/ularkadut', 
-{useNewUrlParser: true, useUnifiedTopology: true}, function() {
-    console.log(`Connected to MongoDB`);
-})
+mongoose.connect('mongodb://localhose:27017/ularkadut',
+    { useNewUrlParser: true, useUnifiedTopology: true }, function () {
+        console.log(`Connected to MongoDB`);
+    })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`App listening on port = ${PORT}`);
 })
