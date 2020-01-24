023d2acb1844fff31a9002528b4e4f4c07377716
dedehudianto12 <dedehudianto12@gmail.com>
2020-01-24 13:33:20 +0700

    resolve confilct

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ const routes = require('./routes')
 const errorHandler = require('./middlewares/errorHandler')
 
 io.on('connection', function (socket) {
-    socket.on('SEND_MESSAGE', function (data) {
-        io.emit('MESSAGE', data)
-    });
+	socket.on('SEND_MESSAGE', function (data) {
+		io.emit('MESSAGE', data)
+	});
 });
 
 app.use(cors());
@@ -22,22 +22,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/ularkadut',
-    {
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log(`Connected to MongoDB`);
+	{
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(() => {
+		console.log(`Connected to MongoDB`);
 
-    }).catch((err) => {
-        console.log((err));
-    })
+	}).catch((err) => {
+		console.log((err));
+	})
 
 app.use('/', routes)
 app.use(errorHandler)
 
 server.listen(PORT, () => {
-    console.log(`App listening on port = ${PORT}`);
+	console.log(`App listening on port = ${PORT}`);
 })
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@
 
 const routes = require('express').Router()
 const userRouter = require('./user')
+const roomRouter = require('./room');
 
+routes.use('/', roomRouter);
 routes.use('/users', userRouter)
 
 module.exports = routes
